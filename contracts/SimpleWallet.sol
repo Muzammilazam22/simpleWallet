0bf7cc93dd354723efc98273fd5027ecd441c352
muzammil <muzammil.azam88@gmail.com>
2021-05-17 14:11:07 +0500

    first commit

---
+++ contracts/SimpleWallet.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.8.4;
+//SPDX-License-Identifier: MIT
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract SimpleWallet is Ownable {
+    mapping(address => uint256) public allowance;
+
+    function addAllowance(address _who, uint256 _amount) public onlyOwner {
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint256 _amount) {
+        require(
+            msg.sender == owner() || allowance[msg.sender] >= _amount,
+            "You are not Allowed"
+        );
+        _;
+    }
+
+    function reduceAllowance(address _who, uint256 _amount) internal {
+        allowance[_who] -= _amount;
+    }
+
+    function withdrawMoney(address payable _to, uint256 _amount)
+        public
+        ownerOrAllowed(_amount)
+    {
+        require(
+            _amount <= address(this).balance,
+            "There are not enough funds stored in the smart contracts"
+        );
+        if (msg.sender != owner()) {
+            reduceAllowance(msg.sender, _amount);
+        }
+        _to.transfer(_amount);
+    }
+
+    fallback() external payable {}
+
+    receive() external payable {}
+}
